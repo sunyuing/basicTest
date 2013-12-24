5d2bc3773f6deb938e27e83606edc8d4b6d1577f
sunyuing <sun.yui.ng@gmail.com>
2013-12-23 23:54:36 -0800

    Implementation of calling real API

--- basicTest/src/test/java/com/jng/basicTest/APITestStepDefinitions.java
+++ basicTest/src/test/java/com/jng/basicTest/APITestStepDefinitions.java
@@ -1,66 +1,120 @@
 package com.jng.basicTest;
 
-import cucumber.api.PendingException;
+import java.util.HashMap;
+import java.util.Map;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
 
-import com.sun.jersey.api.client.*;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.*;
-import com.sun.jersey.core.util.MultivaluedMapImpl;
 
-import javax.ws.rs.core.MultivaluedMap;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import static org.junit.Assert.assertTrue;
+
+import javax.ws.rs.core.MediaType;
 
 public class APITestStepDefinitions {
 
-	private Client client;
 	private String authToken;
 	private String userID;
+	private String empID;
+	private ClientResponse response;
+
 	private static final String APIKey = "95144D49-2EF1-46B0-85A7-CDD0C6F259EE";
 
+	@SuppressWarnings("unchecked")
 	@Given("^I authenticate with web service$")
 	public void I_authenticate_with_web_service() throws Throwable {
-		// Express the Regexp above with the code you wish you had
-        //ClientConfig cc = new DefaultClientConfig();
-        //cc.getProperties().put(ClientConfig.PROPERTY_FOLLOW_REDIRECTS, true);
-        //Client client = Client.create(cc);
-        Client client = Client.create();
-        
-        
-//		WebResource webResource = client.resource("http://ares.concerogroup.com/CTApiServiceTest/v1/authenticate");
-//        MultivaluedMap formData = new MultivaluedMapImpl();
-//		formData.add("email", "a.afonso@tw88.com");
-//		formData.add("password", "tailorwell1#");
-//		ClientResponse response = webResource.type("application/x-www-form-urlencoded").header("X_CONCERO_API_KEY", APIKey).header("X_WORKSPACE_KEY", "demo1").post(ClientResponse.class, formData);
-
-        WebResource webResource = client.resource("http://www.google.com");
-//        WebResource webResource = client.resource("http://ares.concerogroup.com/CTApiServiceTest/v1/authenticate");
 		try {
-	        String s = webResource.get(String.class);
-			System.out.println("OK");
-		}
-		catch (Exception e)
-		{
+			ClientConfig cc = new DefaultClientConfig();
+			cc.getProperties()
+					.put(ClientConfig.PROPERTY_FOLLOW_REDIRECTS, true);
+			Client client = Client.create(cc);
+			// Client client = Client.create();
+
+			WebResource webResource = client
+					.resource("http://ares.concerogroup.com/CTApiServiceTest/v1/authenticate");
+			String input = "{\"email\":\"a.afonso@tw88.com\", \"password\":\"tailorwell1#\" }";
+			response = webResource.type("application/json")
+					.header("X_CONCERO_API_KEY", APIKey)
+					.header("X_WORKSPACE_KEY", "demo1")
+					.accept(MediaType.APPLICATION_JSON)
+					.post(ClientResponse.class, input);
+
+			if (response.getStatus() != 200)
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ response.getStatus());
+
+			String output = response.getEntity(String.class);
+			// System.out.println("Output from Server .... \n");
+			// System.out.println(output);
+
+			Map<String, String> map = new HashMap<String, String>();
+			ObjectMapper mapper = new ObjectMapper();
+			map = mapper.readValue(output, HashMap.class);
+
+			empID = map.get("employeeId");
+			authToken = map.get("X_CONCERO_AUTH_TOKEN");
+			userID = map.get("X_CONCERO_USER_ID");
+
+		} catch (Exception e) {
 			e.printStackTrace(System.out);
 		}
 	}
 
-	@When("^I retrieve the results$")
+	@When("^I retrieve the employee data$")
 	public void I_retrieve_the_results() throws Throwable {
-		// Express the Regexp above with the code you wish you had
-		StringBuilder sb = new StringBuilder();
-		sb.append("Post");
-		System.out.println(sb);
-		// throw new PendingException();
+		try {
+			ClientConfig cc = new DefaultClientConfig();
+			cc.getProperties()
+					.put(ClientConfig.PROPERTY_FOLLOW_REDIRECTS, true);
+			Client client = Client.create(cc);
+
+			WebResource webResource = client
+					.resource("http://ares.concerogroup.com/CTApiServiceTest/v1/employees/"
+							+ empID);
+			response = webResource.type("application/json")
+					.header("X_CONCERO_API_KEY", APIKey)
+					.header("X_CONCERO_AUTH_TOKEN", authToken)
+					.header("X_CONCERO_USER_ID", userID)
+					.header("X_WORKSPACE_KEY", "demo1")
+					.accept(MediaType.APPLICATION_JSON)
+					.get(ClientResponse.class);
+
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
 	}
 
+	@SuppressWarnings("unchecked")
 	@Then("^the status code should be (\\d+)$")
 	public void the_status_code_should_be(int arg1) throws Throwable {
-		// Express the Regexp above with the code you wish you had
-		StringBuilder sb = new StringBuilder();
-		sb.append("Check Result");
-		System.out.println(sb);
-		// throw new PendingException();
+		try {
+			if (response.getStatus() != 200)
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ response.getStatus());
+
+			String output = response.getEntity(String.class);
+			System.out.println("Output from Server .... \n");
+
+			Map<String, String> map = new HashMap<String, String>();
+			ObjectMapper mapper = new ObjectMapper();
+			map = mapper.readValue(output, HashMap.class);
+			System.out.print(map);
+
+			assertTrue("The expected status code was " + arg1
+					+ ", but actually was: " + response.getStatus(),
+					response.getStatus() == arg1);
+
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
+
 	}
 
 }
--- basicTest/src/test/resources/APITest.feature
+++ basicTest/src/test/resources/APITest.feature
@@ -2,6 +2,6 @@ Feature: Test API
 
 Scenario: Test API
 	Given I authenticate with web service
-	When I retrieve the results
+	When I retrieve the employee data
 	Then the status code should be 200
 
